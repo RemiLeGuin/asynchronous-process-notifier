450b860898bc4c77ea22e6ed0bfb206c2aa09ba3
Rémi Le Guin <remileguin@live.fr>
2020-03-07 16:46:20 +0100

    LWC version of the Event Catcher

---
+++ platform-events/main/default/lwc/eventCatcherLwc/eventCatcherLwc.js
@@ -0,0 +1,75 @@
+import { LightningElement, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { subscribe, onError } from 'lightning/empApi';
+import USER_ID from '@salesforce/user/Id';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class EventCatcherLWC extends LightningElement {
+
+    channelName = '/event/AsynchronousProcess__e';
+    subscription = {};
+    recordId;
+
+    @wire(getRecord, { recordId: '$recordId', layoutTypes: 'Full' })
+    record;
+
+    // Called when the component is initialized.
+    // Subscribes to the channel and displays a toast message.
+    // Specifies an error handler function for empApi.
+    connectedCallback() {
+        // Callback function to be passed in the subscribe call after an event is received.
+        // This callback prints the event payload to the console.
+        // A helper method displays the message in the console app.
+        var self = this;
+        const messageCallback = function(response) {
+            self.onReceiveEvent(response);
+        };
+        // Subscribe to the channel and save the returned subscription object.
+        subscribe(this.channelName, -1, messageCallback).then(response => {
+            this.subscription = response;
+        });
+    }
+
+    // Client-side function that displays the platform event message
+    // in the console app and displays a toast if not muted.
+    onReceiveEvent(response) {
+        if (response.data.payload.UserToNotify__c == USER_ID) {
+            // Extract notification from platform event.
+            const newNotification = {
+                recordId: response.data.payload.RecordId__c,
+                recordName: response.data.payload.RecordName__c,
+                message: response.data.payload.Message__c
+            };
+            // Display notification in a toast.
+            this.displayToast("sticky", "error", newNotification.recordId, newNotification.recordName, newNotification.message);
+            // Get record's data.
+            this.recordId = newNotification.recordId;
+            // Refresh record to see data cancellation.
+            refreshApex(this.record);
+        }
+    }
+
+    // Displays the given toast message.
+    displayToast(mode, variant, recordId, recordName, message) {
+        const toastEvent = new ShowToastEvent({
+            "mode": mode,
+            "variant": variant,
+            "message": message,
+            "messageData": [
+                {
+                    url: "/lightning/r/Contact/" + recordId + "/view",
+                    label: recordName,
+                }
+            ]
+        });
+        this.dispatchEvent(toastEvent);
+    }
+
+    // Define an error handler function that prints the error to the console.
+    registerErrorListener() {
+        onError(error => {
+            console.log(JSON.stringify(error));
+        });
+    }
+
+}
